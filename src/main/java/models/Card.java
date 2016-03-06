2648916f772f76200186297cae8d30514a702810
natt8 <suderman@oregonstate.edu>
2016-03-06 11:05:05 -0800

    card class created

--- src/main/java/models/Card.java
+++ src/main/java/models/Card.java
@@ -1,6 +1,12 @@
 package models;
 
 public class Card{
+    public String suit;
+    public String rank;
+    Card(String s, String r) {
+        suit = s;
+        rank = r;
+    }
 
 
 }
---
+++ src/main/java/models/Suit.java
@@ -0,0 +1,8 @@
+package models;
+
+/**
+ * Created by natalie school on 3/6/2016.
+ */
+public class Suit {
+
+}
